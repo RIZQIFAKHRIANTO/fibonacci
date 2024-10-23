67ae8f406b524fc2889bf494da06c18d09cc1933
RIZQIFAKHRIANTO <risqifakhrianto@gmail.com>
2024-10-23 13:11:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan jumlah bilangan Fibonacci yang ingin ditampilkan: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Memanggil fungsi Fibonacci untuk menampilkan deret
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    // Fungsi rekursif untuk menghitung Fibonacci
+    static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
